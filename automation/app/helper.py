a232576659ccf573443824ec11a06a35ff7e800f
Manish Chaudhary <42365690+manishchaudhary101@users.noreply.github.com>
2023-05-31 22:08:36 +0530

    Update helper.py

--- automation/app/helper.py
+++ automation/app/helper.py
@@ -1,13 +1,9 @@
 from .config import *
 import smtplib
+import os
 from email.mime.text import MIMEText
-from pymongo import MongoClient
 import secrets
 import string
-
-
-#generating random password   
-
 
 def generate_random_password():
     length = 16
@@ -17,40 +13,24 @@ def generate_random_password():
         if any(char.isdigit() for char in password):
             return password
 
-def create_iam_user_email_body(username, password, profile: Stack):
-    return f"Hi {username.split('@')[0]},\n\nYour IAM user credentials for {profile.name}, are as follows:\n\nUsername: {username}\nPassword: {password}\n\nPlease use the following link to log in to the AWS Management Console: {get_aws_account_url(profile)}\n"
-
-def create_mongo_user_email_body(username, password, profile: MONGO):
-    return f"Hi {username.split('@')[0]},\n\nYour MongoDB user credentials are as follows for {profile.name}:\n\nUsername: {username}\nPassword: {password}\n"
-
-# def sending_mail(username, password, profile):
-#     if profile in [Stack.JOVEO_PROD, Stack.JOVEO_STAGE, Stack.JOVEO_MGMT, Stack.JOBCLOUD_PROD, Stack.JOBCLOUD_STAGE, Stack.JOBCLOUD_MGMT]:
-#         email_body = create_iam_user_email_body(username, password, profile)
-#     elif profile in [MONGO.TRACKING_CG_MONGO_PROD]:
-#         email_body = create_mongo_user_email_body(username, password, profile)
-#     else:
-#         raise Exception("Unknown profile")
+def sending_mail(username, msg, profile):
     
-#     
-#     
-#     receiver_email = "manish.kumar@joveo.com"  #f"{user}" #f"{user}@example.com"
-#     subject = f'[{profile.name}] User Credentials'
-#     message = MIMEText(email_body)
-#     message['From'] = sender_email
-#     message['To'] = receiver_email
-#     message['Subject'] = subject
+    email_body = msg
+    sender_email = os.getenv("GMAIL_ID")
+    sender_password = os.getenv("GMAIL_TOKEN")
+    receiver_email = f"{username}" #f"{user}@example.com"
+    subject = f'[{profile.name}] User Credentials'
+    message = MIMEText(email_body)
+    message['From'] = sender_email
+    message['To'] = receiver_email
+    message['Subject'] = subject
 
-#     # Send the email
-#     server = smtplib.SMTP('smtp.gmail.com', 587)
-#     server.starttls()
-#     server.login(sender_email, sender_password)
-#     server.sendmail(sender_email, receiver_email, message.as_string())
-#     server.quit()
-
-def get_mongo_connection(hostname, user, passwd):
-    mongoclient = MongoClient(hostname, username=user, password=passwd)
-    db = mongoclient.admin
-    return db
+    # Send the email
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(sender_email, sender_password)
+    server.sendmail(sender_email, receiver_email, message.as_string())
+    server.quit()
 
 
 
